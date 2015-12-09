bd275664a6a50caa3b2ad06988deeb0439dfb646
jaredkitade <jkitade@cca.edu>
2015-12-09 13:25:22 -0800

    site

---
+++ index.php
@@ -0,0 +1,274 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Type Final</title>
+	<link rel="shortcut icon" type="img/png" href="images/favicon3.ico">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">    
+</head>
+
+<body>
+	<!-- --------------------------------------INTRODUCTION--------- -->	
+	<div class='tackle'><img src="hs-tackle-bw.jpg"></div>
+	<div class='body-text' style='margin-top: 80px;'>
+		<p>
+			From high school, to college, all the way to the NFL, football is the largest and most popular sport in the United States. There are over one million students who participate in high school football, nearly twice the amount who participate in the next most popular sport, track and field.
+		</p>
+	</div>
+	<div class='body-text' style='margin-bottom: 40px;'>
+		<p>
+			Football is also arguably the most dangerous sport in the country. There are nearly three times as many injuries in high school football then in any other sport. Former NFL players have shortened life spans along with a reduced quality of life. And most jarringly, even though it is recognized that high school sports are the safest they have ever been, over twelve high school football players die every year.
+		</p>
+	</div>
+	<!-- --------------------------------------CARSON QUOTE--------- -->	
+	<div class='quote'>
+		<div class='quote-photo'><img src='carson.jpg'></div>
+		<div class='title'>
+			<h2>&ldquo;I cannot in good conscience<br>allow my grandson to play<br>knowing what I know.&rdquo;</h2>
+			<div class='person'><span class='name'>Harry Carson</span><br>Pro Football Hall of Fame Linebacker</div>
+		</div>
+	</div>
+	<div class='body-text' style='margin-bottom: 80px;'>
+		<p>
+			It is telling that men who have reached the highest level of fame and success in the sport openly acknowledge that football is not a path that they would willingly allow their loved ones to pursue. Something must change in order to protect the children playing this sport.
+		</p>
+	</div>
+	<!-- --------------------------------------BRIDGEWATER GIF--------- -->	
+	<div class='gif' id='bridgewater'>
+		<div class='overlay'></div>
+	</div>
+	<!-- --------------------------------------MAP TEXT--------- -->	
+	<div class='break-page'>
+		<div class='title'>
+			<h1>Over fifteen high school football players die each year.</h1>
+		</div>
+	</div>
+	<div class='body-text' style='margin-top: 80px;'>
+		<p>
+			According to a study done by the University of North Carolina, two-thirds of all football related deaths (high school, college, semi-pro) between 1931–2014 occurred at the high school level. Head trauma and heat illness are the leading causes of death amongst these players. 
+		</p>
+	</div>
+	<div class='body-text'>
+		<p>
+			Most deaths related to heat stroke occur during preseason workouts. Two-a-days, practicing twice in the same day, have been called risky by experts. These practices often occur during the hottest months of the year and players are oftentimes not acclimated to the heat and amount of strenuous activity. Two-a-days are prohibited in the NFL and heavily regulated at the college level. However, in high school, they are still fairly common, especially in southern states.
+		</p>
+	</div>
+	<div class='body-text' style='margin-bottom: 80px;'>
+		<p>
+			This map shows each reported death in youth football from 2004&ndash;2015. The color of each state represents the average summer temperature in which players practice. Hover over each circle to learn more about each player.
+		</p>
+	</div>
+	<!-- --------------------------------------MAP--------- -->	
+	<div class='map-container'>
+		<img src="map.png">
+		<div class='map-title'>
+			<h3>High School<br>Football Fatalities</h3>
+		</div>
+	<!-- --------------------------------------LOCATIONS--------- -->	
+		<div class='location alejandro'>
+			<div class="ring"></div>
+		</div>
+		<div class='location barajas'>
+			<div class="ring"></div>
+		</div>
+		<div class='location barden'>
+			<div class="ring"></div>
+		</div>
+		<div class='location benson'>
+			<div class="ring"></div>
+		</div>
+		<div class='location bosse'>
+			<div class="ring"></div>
+		</div>
+		<div class='location bui'>
+			<div class="ring"></div>
+		</div>
+		<div class='location coen'>
+			<div class="ring"></div>
+		</div>
+		<div class='location colby'>
+			<div class="ring"></div>
+		</div>
+		<div class='location craig'>
+			<div class="ring"></div>
+		</div>
+		<div class='location davison'>
+			<div class="ring"></div>
+		</div>
+		<div class='location earley'>
+			<div class="ring"></div>
+		</div>
+		<div class='location gomez'>
+			<div class="ring"></div>
+		</div>
+		<div class='location gfeller'>
+			<div class="ring"></div>
+		</div>
+		<div class='location lippo'>
+			<div class="ring"></div>
+		</div>
+		<div class='location matthews'>
+			<div class="ring"></div>
+		</div>
+		<div class='location maughan'>
+			<div class="ring"></div>
+		</div>
+		<div class='location odonnell'>
+			<div class="ring"></div>
+		</div>
+		<div class='location oneill'>
+			<div class="ring"></div>
+		</div>
+		<div class='location pennerman'>
+			<div class="ring"></div>
+		</div>
+		<div class='location d-puente'>
+			<div class="ring"></div>
+		</div>
+		<div class='location s-puente'>
+			<div class="ring"></div>
+		</div>
+		<div class='location quaresma'>
+			<div class="ring"></div>
+		</div>
+		<div class='location d-smith'>
+			<div class="ring"></div>
+		</div>
+		<div class='location snakenberg'>
+			<div class="ring"></div>
+		</div>
+		<div class='location stiles'>
+			<div class="ring"></div>
+		</div>
+		<div class='location socha'>
+			<div class="ring"></div>
+		</div>
+		<div class='location stinnett'>
+			<div class="ring"></div>
+		</div>
+		<div class='location swank'>
+			<div class="ring"></div>
+		</div>	
+		<div class='location turman'>
+			<div class="ring"></div>
+		</div>	
+		<div class='location white'>
+			<div class="ring"></div>
+		</div>	
+		<div class='location will'>
+			<div class="ring"></div>
+		</div>
+		<div class='location willis'>
+			<div class="ring"></div>
+		</div>
+		<div class='location zaragoza'>
+			<div class="ring"></div>
+		</div>
+		<div class='location will'>
+			<div class="ring"></div>
+		</div>
+	</div>
+	<!-- --------------------------------------INJURIES--------- -->	
+	<div class='break-page'>
+		<div class='title'>
+			<h1>Nearly half of all high school athlete injuries are football related.</h1>
+		</div>
+	</div>	
+	<div class='body-text' style='margin-top: 80px;'>
+		<p>
+			A concussion is the most common type of traumatic brain injury. It is the result of a violent hit that causes the brain to hit the sides of the skull. 
+		</p>
+	</div>
+	<div class='table' style='margin-bottom: 80px;'>
+		<img src='table.png'>
+	</div>
+	<!-- --------------------------------------CONCUSSIONS--------- -->	
+	<div class='break-page'>
+		<div class='title'>
+			<h1>Football hits can generate over 290G&rsquo;s of force, nearly 300 times the force of gravity.</h1>
+		</div>
+	</div>	
+	<div class='gif' id='keenum'>
+		<div class='overlay'></div>
+	</div>
+	<div class='body-text' style='margin-top: 80px;'>
+		<p>
+			A concussion is the most common type of traumatic brain injury. It is the result of a violent hit that causes the brain to hit the sides of the skull. 
+		</p>
+	</div>
+	<div class='body-text'  style='margin-bottom: 40px;'>
+		<p>
+			When diagnosed properly, concussions are not life threatening. The danger in football, however, is that many concussions are either unreported or untreated.
+		</p>
+	</div>
+	<div class='quote'>
+			<div class='quote-photo'><img src='coxson2.jpg'></div>
+			<div class='title'>
+				<h2>
+					“He explained to me that the next hit could possibly kill me...I instantly said to myself, ‘I’m done’.”
+				</h2>
+				<div class='person'>
+					<a href='http://www.theplayerstribune.com/adrian-coxson-packers-concussion/'><span class='name'>Adrian Coxson</span></a><br>Former NFL player who retired two weeks into rookie training camp
+				</div>
+			</div>
+		</div>
+	<div class='body-text' style='margin-bottom: 80px;'>
+		<p>
+			Second impact syndrome (SIS) occurs when a previously concussed person suffers another brain injury before the first one completely heals. This can lead to severe brain swelling and is oftentimes fatal. Teenagers are at a much greater risk of suffering from SIS&mdash;all documented cases, outside of boxing, have occured in adolescents. 
+		</p>
+	</div>
+	<!-- --------------------------------------DIAGRAM--------- -->	
+	<div class='break-page'>
+		<div class='title'>
+			<h1>US men have a life expectancy of 78.7 years. NFL players have an average lifespan of 55 years.</h1>
+		</div>
+	</div>	
+	<div class='body-text' style='margin-top: 80px; margin-bottom: 40px';>
+		<p>
+			The average life span for men in the United states is 78.7 years. However, the life expectancy for men who have played in the National Football League drops dramatically to just 55 years. Therefore, NFL players are expected to die nearly 24 years younger based on their chosen profession. Along with a much shorter life span, former NFL players are five times more likely to develop any form of mild cognitive impairment. They are also four times more likely to die from Amyotropic Lateral Sclerosis (ALS or Lou Gehrig’s Disease) or Alzheimer’s Disease.
+		</p>
+	</div>
+	<div class='quote'>
+		<div class='quote-photo'><img src='marshallbw.jpg'></div>
+		<div class='title'>
+			<h2>
+				“You’ll wish your teams<!--  would’ve practiced a lot smarter, and  -->...took better care of the players’ bodies, because the damage you’ll inflict on each other will add up’”
+			</h2>
+			<div class='person'>
+				<span class='name'>Leonard Marshall</span><br>Retired NFL Defensive Lineman 
+			</div>
+		</div>
+	</div>
+	<div class='body-text'>
+		<p>
+			Chronic Traumatic Encephalopathy (CTE) is a degenerative brain disease that is found in people who have suffered repetitive brain trauma. 91 deceased former NFL players of various ages have allowed their brains to be examined by scientists. 87 of them tested positive for CTE, which is 96% of the sample. 
+		</p>
+	</div>
+	<div class='body-text'>
+		<p>
+			CTE, along with Alzheimer’s, ALS, and dementia, are common diagnoses among former NFL players, They are often diagnosed with early-onset versions, which means they develop symptoms at a much younger age. These diseases have symptoms ranging from mild memory loss to complete loss of motor skills.
+		</p>
+	</div>
+	<div class='body-text' style='margin-bottom: 80px;'>
+		<p>
+			This timeline shows the lifespans of 53 former NFL players—22 deceased and 31 still living. All of these players have been diagnosed with or shown symptoms consistent with brain disease. Hover over each bar to learn more information about each player.
+		</p>
+	</div>
+	<div class='diagram'>
+		<img src='diagram.png'>
+	</div>
+	<div class='break-page'>
+		<div class='title'>
+			<h1>Is there a solution?</h1>
+		</div>
+	</div>
+	<div class='body-text' style='margin-top: 80px;'>
+		<p>
+			Football is an inherently violent sport&mdash;there is simply no way to guarantee a player&rsquo;s safety. However, there are ways to minimize the risk of danger.
+		</p>
+	</div>
+</body>
+
+</html>
